06eafbc2ec28895555a77bbf16659b24bafd62bf
Turki <turki@MacBook-Air-Turki.local>
2026-03-28 23:32:50 +0300

    Add Render static site config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Rick/' : '/',
+  base: command === 'build' ? (process.env.RENDER ? '/' : '/Rick/') : '/',
   plugins: [react()],
 }))
